4790390f463a9e4176b3a34b48671e01c684e15d
yuanxiaodong <yuanxiaodong@pachiratech.com>
2016-09-26 12:28:19 +0800

    update

--- simple-trational/translate.js
+++ simple-trational/translate.js
@@ -1,17 +1,19 @@
 var traditionlize = require("./traditionlize");
-var fs = require("fs");
 
-var data = JSON.parse(fs.readFileSync(__dirname + "/simple.json").toString());
+traditionlize("simple.json")
+// var fs = require("fs");
 
-for (var i in data) {
-	data[i] = traditionlize(data[i]);
-}
-console.log(data);
+// var data = JSON.parse(fs.readFileSync(__dirname + "/simple.json").toString());
 
-fs.writeFile(__dirname + "/simple_trational.json", JSON.stringify(data, null, 4), function(err, data){
-	if(err){
-		console.error(err);
-	}else{
-		console.log("转为繁体文件成功");
-	}
-})
+// for (var i in data) {
+// 	data[i] = traditionlize(data[i]);
+// }
+// console.log(data);
+
+// fs.writeFile(__dirname + "/simple_trational.json", JSON.stringify(data, null, 4), function(err, data){
+// 	if(err){
+// 		console.error(err);
+// 	}else{
+// 		console.log("转为繁体文件成功");
+// 	}
+// })
