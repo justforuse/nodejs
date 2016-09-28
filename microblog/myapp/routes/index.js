81c741c7a595f4a4af94a821fd870f7575c1d964
yuanxiaodong <yuanxiaodong@pachiratech.com>
2016-09-28 18:33:07 +0800

    express

---
+++ microblog/myapp/routes/index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get("/hello", function(req, res, next){
+	res.render
+})
+module.exports = router;
