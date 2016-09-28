cac91d61217ba99a1486f2e5cd4ca261d0cd2844
justforuse <allenforuse@gmail.com>
2016-09-28 23:01:57 +0800

    jade模板, 展示列表

--- microblog/myapp/routes/index.js
+++ microblog/myapp/routes/index.js
@@ -1,12 +1,44 @@
 var express = require('express');
 var router = express.Router();
 
+var userList = [
+	{
+		name:"allen",
+		address:{
+			province:"heilongjiang",
+			city:"mudanjiang"
+		}
+	},
+	{
+		name:"bob",
+		address:{
+			province:"neimenggu",
+			city:"tongliao"
+		}
+	}
+]
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	res.render('index', { title: 'Express' });
 });
 
-router.get("/hello", function(req, res, next){
-	res.render
+router.get("/hello", function(req, res, next) {
+	res.send("现在时间是: " + new Date());
+});
+
+router.all("/user/:username", function(req,res,next){
+	console.log("username: " + req.params.username);
+	//res.send(req.params.username);
+	next();
+});
+
+router.get("/user/:username", function(req, res, next){
+	res.render('user', { username : req.params.username})
+});
+
+router.get("/userList", function(req, res, next){
+	res.render('userList', {userList : userList});
 })
+
+
 module.exports = router;
